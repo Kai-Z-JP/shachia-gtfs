17d22f516cd0fcbecaa390319fc76ac891386310
Kaiz <kaiz@kaiz.jp>
2024-12-24 20:08:41 +0900

    fix: extract zip failed

--- src/jvmMain/kotlin/jp/kaiz/shachia/gtfs/io/zip/ZipExtractorJvm.kt
+++ src/jvmMain/kotlin/jp/kaiz/shachia/gtfs/io/zip/ZipExtractorJvm.kt
@@ -1,9 +1,40 @@
 package jp.kaiz.shachia.gtfs.io.zip
 
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipEntry as JavaZipEntry
+
 class ZipExtractorJvm : ZipExtractor {
     override suspend fun extract(zipBytes: ByteArray): List<ZipEntry> {
-        val zipReader = ZipReader(zipBytes)
-        return zipReader.readEntries()
+        val zis = ZipInputStream(zipBytes.inputStream())
+        val zipEntries = zis.map {
+            val fileName = it.name
+            val compressedSize = it.compressedSize
+            val uncompressedSize = it.size
+            val compressionMethod = it.method
+
+            val data = zis.readBytes()
+
+            ZipEntry(
+                fileName = fileName,
+                compressedSize = compressedSize,
+                uncompressedSize = uncompressedSize,
+                compressionMethod = compressionMethod,
+                data = data
+            )
+        }
+        return zipEntries
+    }
+
+    fun <R> ZipInputStream.map(transform: (JavaZipEntry) -> R): List<R> {
+        val entries = mutableListOf<R>()
+        var entry = nextEntry
+        while (entry != null) {
+            val r = transform(entry)
+            entries.add(r)
+            entry = nextEntry
+        }
+
+        return entries
     }
 }
 
